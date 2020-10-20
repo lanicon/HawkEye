c698815ae1c1bd8371c088ca3ba8085e4dcd72fc
Kitt3120 <torben.schweren@gmail.com>
2020-10-20 14:52:03 +0200

    More license notes
    Added license in Program class
    License note will now be printed when the program starts

--- HawkEye/Program.cs
+++ HawkEye/Program.cs
@@ -8,6 +8,28 @@ using System.Threading.Tasks;
 
 namespace HawkEye
 {
+    /*
+    HawkEye: Archive server content indexing tool with OCR
+    Copyright (C) 2020  Torben Schweren
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Need to contact me?
+    Email: torben.schweren@gmail.com
+    Discord: Viper#3408
+    */
+
     internal class Program
     {
         public static bool IsRunning { get; private set; }
@@ -16,6 +38,9 @@ namespace HawkEye
         {
             Thread.CurrentThread.Name = "Main";
             IsRunning = true;
+
+            PrintLicense();
+            Console.Write("\n\n");
 
             Services.Initiate();
 
@@ -31,5 +56,23 @@ namespace HawkEye
         }
 
         public static void Stop() => IsRunning = false;
+
+        private static void PrintLicense()
+        {
+            Console.WriteLine(
+                "HawkEye  Copyright (C) 2020  Torben Schweren" +
+                "\nThis program comes with ABSOLUTELY NO WARRANTY!" +
+                "\nThis is free software, and you are welcome to redistribute it under certain conditions." +
+                "\nSee the LICENSE file for more details about warranty and conditions for redistribution." +
+                "\n" +
+                "\nHawkEye uses the open source Tesseract library for OCR" +
+                "\nwith an open source wrapper made by Charles Weld." +
+                "\nBoth are licensed under the Apache License version 2.0." +
+                "\n" +
+                "\nHawkEye uses the open source DocNet library for parsing PDF files" +
+                "\nwith an open source wrapper made by modestas." +
+                "\nBoth are licensed under the MIT License"
+                );
+        }
     }
 }
